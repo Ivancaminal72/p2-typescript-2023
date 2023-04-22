3b68f9f09981f980c37945c7d88e10b0f9a4b0a8
Ivan Caminal <14839609+Ivancaminal72@users.noreply.github.com>
2023-04-22 20:12:58 +0200

    Creo una app basica que renderiza un fichero pug

--- main.ts
+++ main.ts
@@ -1,4 +1,19 @@
-import { loadUsers } from "./users.js";
+import { Character, loadUsers } from "./users.js";
 
-const users = await loadUsers(100);
+//const users = await loadUsers(100);
 
+const path = require('path');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
+
+app.get('/', (req, res) => {
+    res.render("characters")
+  })
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
