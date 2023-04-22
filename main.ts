bafabbe565d5e86f6675c519f91af4a251670d2d
Ivan Caminal <14839609+Ivancaminal72@users.noreply.github.com>
2023-04-22 20:01:39 +0200

    Instalo express, pug y elimino ficheros

--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
-import { writeFile } from "fs/promises";
-import { render } from "./render.js";
 import { loadUsers } from "./users.js";
 
 const users = await loadUsers(100);
-console.log(users);
-
-//const html = render(users);
-//await writeFile('users.html', html);
 
