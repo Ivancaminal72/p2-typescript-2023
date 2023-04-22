4b564c721a6b4d538467ebc260776b7991ca7bfc
Ivan Caminal <14839609+Ivancaminal72@users.noreply.github.com>
2023-04-22 19:54:38 +0200

    Numero de datos elejible desde main.ts

--- users.ts
+++ users.ts
@@ -9,9 +9,9 @@ export class Character {
   ) {}
 }
 
-export const loadUsers = async () => {
+export const loadUsers = async (num: number) => {
   const characters: Array<Character> = [];
-  for(let i = 0; i < 100; i++){
+  for(let i = 0; i < num; i++){
     const response = await fetch(`https://rickandmortyapi.com/api/character/${i}`);
     const result = await response.json() as any;
     characters.push(new Character(result.name, result.status, result.species, result.gender, result.image));
